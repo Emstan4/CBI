6b6128533a95086689e7c21db7677c4736170c1e
Emstan4 <u12284922@tuks.co.za>
2016-04-12 20:05:23 +0200

    My first commit-adding CBI files

---
+++ CBI_TUT12_d.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  8 14:56:45 2016
+
+@author: user
+"""
+
+from __future__ import division
+from numpy.linalg import solve
+import numpy as np
+from matplotlib import pyplot as plot
+a = 0.12
+b = 0.135
+g = 1.85
+cla = np.arange(0,2.5,.01)
+npoints = len(cla)
+
+mumax = 0.55
+Kp = 4/30
+mu = mumax*(1 + (cla/Kp))**-1
+thetha = 0.2
+
+S = np.matrix([[1+a,     1,      0,      0,      0,      0,      0,      0],
+               [0,      -1,      1,      1,      0,      0,      0,      0],
+               [0,       0,      0,     -1,      1,      1,      0,      0],
+               [0,       0,      0,      0,      0,     -1,      1,      1],
+               [0,       0,      0,      0,      0,   -1/3,      1,      0],
+               [b,       0,   -1/3,    1/3,   -1/3,      0,      0,     -1],
+               [-g,   -1/3,      0,    2/3,      0,      0,      0,      0],
+               [0,       0,      1,      0,      0,      0,      0,      0]])
+#              
+C = np.matrix( [1,       0,      0,      0,      0,      0,      0,      0]).T
+Yg = solve(S, C)
+
+Ysatp_g=-1/3*Yg[1]+2/3*Yg[3]
+
+S[6] = [1,0,0,0,0,0,0,0]
+Ym = solve(S, C)
+Ysatp_m=-1/3*Ym[1]+2/3*Ym[3]
+#rates
+
+r_la = (Yg[4]/Yg[0])*mu + (Ym[4]/Ysatp_m)*thetha
+
+r_et = (Yg[-1]/Yg[0])*mu + (Ym[-1]/Ysatp_m)*thetha
+r_s = (1/Yg[0])*mu + (1/Ysatp_m)*thetha
+
+#observed yields
+Yla_o = np.asarray(r_la/r_s)
+Yla_o = np.reshape(Yla_o, npoints)
+Yet_o = r_et/r_s
+
+plot.plot(cla, Yla_o)
+plot.show()
+
+
---
+++ CBI_fermenter_model.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 06 19:37:44 2016
+
+@author: StudyCentre
+"""
+
+from __future__ import division
+import numpy as np
+from matplotlib import pyplot as plot
+
+
+
+
+
+#   gluc      X         Gly     Lac       Form        Eth        Atp
+Yg = [1,    0.14992504,     0,   0.77136432,   0.02023988,     0.04047976,     0.27736132] # [0 Gluc, 1 X, 2 Gly, 3 Et, 4 CO2, 5 ATP]
+Ym = [1,         0,     0,         1,        0,          0,     1/3] 
+
+Qf = Q = Cxf = Csf = Cgf = Cef = Clacf = Cff = 0                    #For now it is a batch fermenter
+Vo = 500       
+#              X     Glu   Gly  Lac Form etha Vo/Vo             #1000 liter initial volume
+Co = np.array([0.00159, 3.33,  0,   0,   0,   0,   1.0])             #[X, Gluc, Gly, Et and Vo/Vo] at t=0 in cmol/L
+No = Co*Vo                                  #total initial cmol in the fermenter and volume at the end (see below why)
+mumax, thethamax, Km = 0.55 , 0.2, 0.008
+Kp = 4/30
+
+def r_prime(C):
+    
+    Cx, Cs, Cg, Clac, Cf, Ce = C
+    
+    mu = mumax*((1 + (Clac/Kp))**(-1))*(Cs/(Km+Cs))
+    thetha = thethamax#*(Cs/(Km+Cs))
+    r_x = mu
+    r_s = -((Yg[0]/Yg[1])*mu + (Ym[0]/Ym[6])*thetha)
+    r_g = (Yg[2]/Yg[1])*mu + (Ym[2]/Ym[6])*thetha
+    r_l = (Yg[3]/Yg[1])*mu + (Ym[3]/Ym[6])*thetha
+    r_f = (Yg[4]/Yg[1])*mu + (Ym[4]/Ym[6])*thetha
+    r_e = (Yg[5]/Yg[1])*mu + (Ym[5]/Ym[6])*thetha
+    
+    return [r_x, r_s, r_g, r_l, r_f, r_e]
+             
+def dNdt_fun(N,t):
+    Cx, Cs, Cg, Clac, Cf, Ce, V = N[0]/N[6],    N[1]/N[6],  N[2]/N[6],  N[3]/N[6],  N[4]/N[6],  N[5]/N[6],  N[6]  #calculating and naming concentration and volume 
+    r=r_prime([Cx, Cs, Cg,Clac, Cf, Ce])
+    
+    return [Qf*Cxf - Q*Cx + (r[0])*Cx*V,
+            Qf*Csf - Q*Cs + (r[1])*Cx*V,    
+            Qf*Cgf - Q*Cg + (r[2])*Cx*V,
+            Qf*Clacf - Q*Clac + (r[3])*Cx*V,
+            Qf*Cff - Q*Cf + (r[4])*Cx*V,            
+            Qf*Cef - Q*Ce + (r[5])*Cx*V,
+            Qf - Q]  
+            
+tspan = np.arange(0,30,0.001)
+dt = tspan[1]
+Cx_list =  []
+Cs_list =  []
+Cg_list =  []
+
+Cl_list =  []
+Cf_list =  []
+
+Ce_list =  []
+yield_list = []
+for i, t in enumerate(tspan):
+     mat = dNdt_fun(No, t)
+     mat = np.array(mat)
+     N = No + dt*mat
+     No = N
+     Cx, Cs, Cg, Clac, Cf, Ce, V = N[0]/N[6],    N[1]/N[6],  N[2]/N[6],  N[3]/N[6],  N[4]/N[6],  N[5]/N[6],  N[6]
+     r = r_prime([Cx, Cs, Cg,Clac, Cf, Ce])
+     yield_list.append(r[3]/abs(r[1]))
+     #print N[3]
+     Cx_list.append(N[0]/N[6])
+     Cs_list.append(N[1]/N[6])
+     Cg_list.append(N[2]/N[6])
+     Cl_list.append(N[3]/N[6])
+     Cf_list.append(N[4]/N[6])
+     Ce_list.append(N[5]/N[6])
+     
+#print Cl_list[-1]*30.03
+#print yield_list[0]
+#plot.plot(tspan, Ce_list, color='blue', label='e')
+plot.plot(Cl_list, yield_list, color='black', label='yield')
+#plot.plot(tspan, Cs_list, color='red',label='s')
+#plot.plot(tspan, Cx_list, color='green',label='x')
+##
+#plot.plot(tspan, Cl_list, color='black',label='l')
+#plot.plot(tspan, Cf_list, color='yellow',label='f')
+##
+#plot.plot(tspan, Cg_list, color='cyan',label='g')
+#plot.legend(loc='best')
+#plot.ylabel('Concentration cmol/L') 
+#plot.xlabel('time (h)') 
+plot.show()
