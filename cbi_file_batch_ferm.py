4a638a1abda2d700532f9cd8ea8cb4bfa55f8f35
emstan4 <u12284922@tuks.co.za>
2016-05-03 13:39:22 +0200

    TUT-15-j

--- cbi_file_batch_ferm.py
+++ cbi_file_batch_ferm.py
@@ -9,14 +9,17 @@ from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plot
 from scipy.integrate import odeint
+
+
+kq = 0.61
     # X     S     P       ATP
 Yg = [0.25, 1 ,  0.52,    0.23]
 Ym = [0,    1 ,   0.8,    0.333]
 #            Xo So Po Qo Q 
-init_cond = [0, 0, 0, 0, 0]
+init_cond = [0, 12, 0, kq, 0]
 Vo = 5000       
 #                 X          S      P     Vo/Vo             #1000 liter initial volume
-Co = np.array([0.0006,      4.667,    0,      1.0])             #[X, Gluc, Gly, Et and Vo/Vo] at t=0 in cmol/L
+Co = np.array([0.0006,      0.02,    0,      1.0])             #[X, Gluc, Gly, Et and Vo/Vo] at t=0 in cmol/L
 No = Co*Vo
 mumax, thethamax, Km = 0.22 , 0.12, 0.0004
 Ks = 0.5
@@ -24,7 +27,7 @@ Ks = 0.5
 def r_prime(C):
         
     mu = mumax*C[1]/(Km+C[1])*((1 + C[1]/Ks)**(-2))
-    thetha = 0#thethamax*(C[1]/(Km+C[1]))
+    thetha = thethamax*(C[1]/(Km+C[1]))
     r = [mu]
     for i in range(len(Yg)-2):
         r.append((Yg[i+1]/Yg[0])*mu + (Ym[i+1]/Ym[-1])*thetha)        
@@ -40,11 +43,11 @@ def dNdt_fun(N,t):
     rplot.append(r[0])
     var = []
     for i in range(len(r)):        
-        var.append(init_cond[-2]*init_cond[i] - init_cond[-1]*C[i] + r[i]*C[0]*C[-1])
-    var.append(init_cond[-1]-init_cond[-2])
+        var.append(init_cond[-2]*t*init_cond[i] - init_cond[-1]*C[i] + r[i]*C[0]*C[-1])
+    var.append(init_cond[-2]*t - init_cond[-1])
     return var
     
-tspan = np.arange(0,2000,0.05)
+tspan = np.arange(0,130,0.05)
 dt = tspan[1]
 
 N = odeint(dNdt_fun, No, tspan)
@@ -65,25 +68,29 @@ Y_acc = N[:,2]/(No[1] - N[:,1])
 z = (Yg[1]/Yg[0])*rx/rs
 
 
-Cx=N[:,0]/N[:,3]                           #devide cmol amount by the volume to get concentration 
+Cx=N[:,0]/N[:,3]                           #divide cmol amount by the volume to get concentration 
 Cs=N[:,1]/N[:,3]
 Cp=N[:,2]/N[:,3]
+V = N[:,3]
 
-Cs_1430 = np.interp(1430,tspan , Cs)
-x = ((Co[1] - Cs_1430)/Co[1])*100
+time = np.interp(3.548, Cp, tspan)
+prod = 3.548/time
+#sol = np.interp(0.00, C, tspan[::10])
+#x = ((Co[1] - Cs_1430)/Co[1])*100
 #Y_acc_1430= np.interp(1430,tspan , Y_acc)
 #vol_prod = max(Cp)/t_end
 
-print "Conversion after 1430 hrs = ", x  
+print "Time(3.548 cmol/L) = ", time, "hr"
+print "Volumetric production rate:", prod, "cmol/L.h"
 #print "Y accumulated(1430) = ", Y_acc_1430  
 
-#plot.plot(tspan, Cp ,color='red',label='$C_{SA}$')
-#plot.plot(tspan, Cs,color='black',label='$C_{S}$')
-#plot.plot(tspan, Cx,color='blue',label='$C_{X}$')
-#plot.legend(loc='best')
-#plot.ylabel('Concentration cmol/L') 
-#plot.xlabel('time (h)') 
-#plot.show()
+plot.plot(tspan, Cp ,color='red',label='$C_{P}$')
+plot.plot(tspan, Cs,color='black',label='$C_{S}$')
+plot.plot(tspan, Cx,color='blue',label='$C_{X}$')
+plot.legend(loc='best')
+plot.ylabel('Concentration cmol/L') 
+plot.xlabel('time (h)') 
+plot.show()
 #plot.plot(tspan, Y_acc)
 
   
