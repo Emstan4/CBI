8040783559022df4f876b29850bcdaad0239e34f
emstan4 <u12284922@tuks.co.za>
2016-05-05 18:47:58 +0200

    Solving for several steady-states

--- cbi_file_batch_ferm.py
+++ cbi_file_batch_ferm.py
@@ -9,27 +9,21 @@ from __future__ import division
 import numpy as np
 from matplotlib import pyplot as plot
 from scipy.integrate import odeint
+from scipy.optimize import fsolve
 
 
-#kq = 15
-#    # X     S     P       ATP
-#Yg = [0.25, 1 ,  0.52,    0.23]
-#Ym = [0,    1 ,   0.8,    0.333]
-##            Xo So Po Qo Q 
-#init_cond = [0, 12, 0, kq, 0]
-#Vo = 5000       
-##                 X          S      P     Vo/Vo             #1000 liter initial volume
-#Co = np.array([0.0006,      0.0,    0,      1.0])             #[X, Gluc, Gly, Et and Vo/Vo] at t=0 in cmol/L
-#No = Co*Vo
-#mumax, thethamax, Km = 0.22 , 0.12, 0.0004
-#Ks = 0.5
+Csex=np.array([ 4.        ,  3.99809878,  3.99452932,  3.98782783,  3.97524613,
+        3.95162487,  3.90727832,  3.82402459,  3.66773752,  3.37438079,
+        2.82385119])
+
+tex=np.array([  0. ,   4.2,   8.4,  12.6,  16.8,  21. ,  25.2,  29.4,  33.6,
+        37.8,  42. ])
+
 mumax,thethamax, Km, Kp= 0.3, 0.14, 0.0005, 0.5
 #Cxo, Cso = 0.0003, 3.5
 Yg=[0.12, 1, 0.6 , 0.38] 
 Ym=[0, 1, 0.75, 0.46]                     
 Vo=10000                                   
-#Co=np.array([Cxo, Cso, 0, 1])     #[X, S, P, V]             
-#No=Co*Vo
 Cxf=Csf=Cpf=0 
 
 Cso=0
@@ -41,7 +35,7 @@ Csf=3.5                              # substrate in feed
 Cxf=Cpf=0
 init_cond = [0, 3.5, 0, 0, 0]
 def r_prime(C):
-        
+
     mu = mumax*C[1]/(Km+C[1])*((1 + C[2]/Kp)**(-1))
     thetha = thethamax*(C[1]/(Km+C[1]))
     r = [mu]
@@ -50,6 +44,23 @@ def r_prime(C):
     r[1] = -r[1]
     return r
     
+def steady_state(C,Q):               
+    r=r_prime(C)
+    vari = []
+    Qf = Q
+    for i in range(len(r)):  
+        vari.append(Qf*init_cond[i] - Q*C[i] + r[i]*C[0]*Vo)
+    return vari
+
+Qspan = np.arange(200,2500,100)
+Csteady = []
+Co = [0.4,0,2.2]
+for qi in Qspan:           
+    Co=fsolve(steady_state,Co,args=qi)  
+    Csteady.append(Co)#Get estimate from graph above
+#print C_steady
+#plot.plot(Qspan, Csteady,'o') 
+plot.plot(tex,Csex,'o')
 rplot = []    
 def dNdt_fun(N,t):
     C = np.zeros(len(Yg))
@@ -61,12 +72,10 @@ def dNdt_fun(N,t):
     rplot.append(r[0])
     var = []
     for i in range(len(r)):  
-#        Qf = 500#(C[-1]*(r[1]*C[0]-kq))/(C[1]-init_cond[1])
-#        Q = Qf
         var.append(Qf*init_cond[i] - Q*C[i] + r[i]*C[0]*C[-1])
     var.append(Qf - Q)
     return var
-    
+      
 tspan = np.arange(0,150,0.01)
 dt = tspan[1]
 
@@ -103,17 +112,17 @@ prod = 3.548/time
 #Y_acc_1430= np.interp(1430,tspan , Y_acc)
 #vol_prod = max(Cp)/t_end
 #print "Maximum rate:", overall_y
-print "Production rate(0.3548 cmol/L) = ", prod, "cmol/h L"
+#print "Production rate(0.3548 cmol/L) = ", prod, "cmol/h L"
 #print "Volumetric production rate:", prod, "cmol/L.h"
 #print "Y accumulated(1430) = ", Y_acc_1430  
 
-plot.plot(tspan, Cp ,color='red',label='$C_{P}$')
-plot.plot(tspan, Cs,color='black',label='$C_{S}$')
-plot.plot(tspan, Cx,color='blue',label='$C_{X}$')
-plot.legend(loc='best')
-plot.ylabel('Concentration cmol/L') 
-plot.xlabel('time (h)') 
-plot.show()
+#plot.plot(tspan, Cp ,color='red',label='$C_{P}$')
+#plot.plot(tspan, Cs,color='black',label='$C_{S}$')
+#plot.plot(tspan, Cx,color='blue',label='$C_{X}$')
+#plot.legend(loc='best')
+#plot.ylabel('Concentration cmol/L') 
+#plot.xlabel('time (h)') 
+#plot.show()
 #plot.plot(Cp, rprod)
 
   
