03156ae197274cdc394afced7beb86e3e4bb9d2f
emstan4 <u12284922@tuks.co.za>
2016-05-17 10:59:35 +0200

    tut-17-start

--- cbi_file_batch_ferm.py
+++ cbi_file_batch_ferm.py
@@ -12,31 +12,26 @@ from scipy.integrate import odeint
 from scipy.optimize import fsolve
 
 
-Csex=np.array([ 4.        ,  3.99809878,  3.99452932,  3.98782783,  3.97524613,
-        3.95162487,  3.90727832,  3.82402459,  3.66773752,  3.37438079,
-        2.82385119])
 
-tex=np.array([  0. ,   4.2,   8.4,  12.6,  16.8,  21. ,  25.2,  29.4,  33.6,
-        37.8,  42. ])
 
-mumax,thethamax, Km,km2, Kp= 0.15, 0.1, 0.0005,0.0005, 1000
+mumax,thethamax, Km,km2, Kp= 0.38, 0.15, 0.00133,0.0002, 1000
 #Cxo, Cso = 0.0003, 3.5
-Yg=[0.18, 1, 0.5 , 0.3] 
-Ym=[0, 1, 0.65, 0.4]                     
-Vo=1000                                       
+Yg=[0.569, 1,  0.43,   1.32 ] 
+Ym=[0,1,    1,   3.567 ]                    
+Vo=5000                                       
 Cxf=Csf=Cpf=0 
 
-Cso=2.82385119
-Cxo = 0.0003
+Cso=4.167
+Cxo = 0.0126
 Co=np.array([Cxo, Cso, 0, 1])     #[X, S, P, V]             
 No=Co*Vo
-Qf=Q=50                            # define throughflow 
-Csf=4                             # substrate in feed 
+Qf=Q=0                           # define throughflow 
+Csf=0                             # substrate in feed 
 Cxf=Cpf=0
-init_cond = [0, 4, 0, 0, 0]
+init_cond = [0, 0, 0, 0, 0]
 def r_prime(C):
 
-    mu = mumax*C[1]/(Km+C[1])*((1 + C[2]/Kp)**(-1))
+    mu = mumax*C[1]/(Km+C[1])#*((1 + C[2]/Kp)**(-1))
     thetha = thethamax*(C[1]/(km2+C[1]))
     r = [mu]
     for i in range(len(Yg)-2):
@@ -51,22 +46,22 @@ def steady_state(C,Q):
     for i in range(len(r)):  
         vari.append(Qf*init_cond[i] - Q*C[i] + r[i]*C[0]*Vo)
     return vari
-Qspan = np.arange(1,200,1)
-D_list = Qspan/Vo
-Csteady = []
-Co = [0.4,0,2.2]
-P_list = []
-for qi in Qspan:           
-    Co=fsolve(steady_state,Co,args=qi) 
-    Ysp_o=Co[2]/(Csf-Co[1])
-    P = Co[2]#*qi/Vo
-    P_list.append(Ysp_o)
-    Csteady.append(Co)
+#Qspan = np.arange(1,200,1)
+#D_list = Qspan/Vo
+#Csteady = []
+#Co = [0.4,0,2.2]
+#P_list = []
+#for qi in Qspan:           
+#    Co=fsolve(steady_state,Co,args=qi) 
+#    Ysp_o=Co[2]/(Csf-Co[1])
+#    P = Co[2]#*qi/Vo
+#    P_list.append(Ysp_o)
+#    Csteady.append(Co)
 
-Csteady = np.array(Csteady)
-plot.plot(D_list, P_list) 
-D = np.interp(0.01,D_list,P_list)
-print D
+#Csteady = np.array(Csteady)
+#plot.plot(D_list, P_list) 
+#D = np.interp(0.01,D_list,P_list)
+#print D
 rplot = []    
 def dNdt_fun(N,t):
     C = np.zeros(len(Yg))
@@ -82,7 +77,7 @@ def dNdt_fun(N,t):
     var.append(Qf - Q)
     return var
       
-tspan = np.arange(0,500,0.01)
+tspan = np.arange(0,100,0.1)
 dt = tspan[1]
 
 N = odeint(dNdt_fun, No, tspan)
@@ -104,16 +99,18 @@ Y_acc = N[:,2]/(No[1] - N[:,1])
 #print Y_acc[-1]
 z = (Yg[1]/Yg[0])*rx/rs
 
-y_obs = z*Yg[2] + (1-z)*Ym[2]
+#y_obs = z*Yg[2] + (1-z)*Ym[2]
 Cx=N[:,0]/N[:,3]                           #divide cmol amount by the volume to get concentration 
 Cs=N[:,1]/N[:,3]
 Cp=N[:,2]/N[:,3]
-V = N[:,3]
-rprod = Cx*rp
-time = np.interp(3.548, Cp, tspan)
-overall_y = np.interp(3.548, Cp, y_obs)
-prod = 3.548/time
-#sol = np.interp(0.00, C, tspan[::10])
+#V = N[:,3]
+#rprod = Cx*rp
+#time = np.interp(3.548, Cp, tspan)
+#overall_y = np.interp(3.548, Cp, y_obs)
+#prod = 3.548/time
+sol = np.interp(0.00001, Cs[::-1], tspan[::-1])
+print (max(Cx)/sol)*23.9
+#print sol
 #x = ((Co[1] - Cs_1430)/Co[1])*100
 #Y_acc_1430= np.interp(1430,tspan , Y_acc)
 #vol_prod = max(Cp)/t_end
