f854f2bc4c63de325149ab9a55a64b7c22b00c7a
emstan4 <u12284922@tuks.co.za>
2016-05-26 17:57:02 +0200

    tut-18-c

--- cbi_file_batch_ferm.py
+++ cbi_file_batch_ferm.py
@@ -13,28 +13,35 @@ from matplotlib import pyplot as plot
 a = .1
 b = .1
 g = 2.5
+mu = 0.12
+theta = 0.123
+lambda_max = 0.24245455
 
 Sg = np.matrix([[1+a,1,0.75,0],
                 [b,2,0,-2],
                 [-g,2/3,0,3],
-                [0,0,1,0]])   
-Cg = np.matrix([[1,0,0,0]]).T                
+                [1,0,0,0]])   
+Cg = np.matrix([[1,0,theta/lambda_max,0]]).T                
 Yg = np.linalg.solve(Sg,Cg)
 
 Yxs_g = 1/Yg[0]
 
+#print Yg#[2]*(33.5/30)
 
-
-Sg[2] = [1,0, 0, 0]       
+#Sg[2] = [1,0, 0, 0]       
 Yg = np.linalg.solve(Sg,Cg)
+Yatp_sm = (2/3)*Yg[1] + 3*Yg[-1]
 
+lambda_max = Yxs_g*mu + (1/Yatp_sm)*theta
+#print lambda_max
+print Yg[2][0,0]*(33.5/30)
+#print Yxs_g, Yatp_sm
 #print Yg  
 dt = 0.1
 tspan = np.arange(0,50,dt)
 Cx = []
 Cs = []
-mu = 0.12
-theta = 0.123
+
 tex = [0,48]
 cx_ex = [0.03/24.9, 9.515/24.9]
 cs_ex = [100/30, 76.63/30]
@@ -44,4 +51,4 @@ csi = cs_ex[0]
 #maximum possible glycolyctic flux
 
 r_max = (Yxs_g[0,0]*mu + (1/Yatp_sm[0,0])*theta)*(30/24.6)
-print r_max
+#print r_max
